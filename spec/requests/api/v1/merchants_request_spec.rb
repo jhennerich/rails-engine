6a7d5a8f49487956864e0f2c9222028205c34e77
John Hennerich <john.hennerich@gmail.com>
2022-05-17 17:57:33 -0600

    Feat: implement api merchants call, tests pass

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,5 +5,19 @@ RSpec.describe 'The merchants API' do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    merchants = response_body[:data]
+
+    expect(response).to be_successful
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(String)
+      expect(merchant[:type]).to eq('merchant')
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
   end
 end
