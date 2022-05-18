f3a9ac39987db8d94f26b719aa2217b246bd5b29
John Hennerich <john.hennerich@gmail.com>
2022-05-17 18:59:36 -0600

    Feat: implement merchant api call returns item data

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -22,12 +22,42 @@ RSpec.describe 'The merchants API' do
   end
 
   it 'sends data for one merchant given id' do
-    merchant = create(:merchant)
-    get "/api/v1/merchants/#{merchant.id}"
+    create_list(:merchant, 2)
+    merchant1 = Merchant.first
+    merchant2 = Merchant.last
+
+    get "/api/v1/merchants/#{merchant1.id}"
 
     merchant = JSON.parse(response.body, symbolize_names: true)
+
     expect(response).to have_http_status(:ok)
+    expect(response).to be_successful
+
+    expect(merchant.count).to eq(1)
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:type]).to eq('merchant')
     expect(merchant[:data][:attributes]).to include(:name)
-    expect(merchant[:data][:attributes][:name]).to be_a(String)
+    expect(merchant[:data][:attributes][:name]).to eq(merchant1.name)
+    expect(merchant[:data][:id]).to_not eq(merchant2.id)
+  end
+
+  it 'sends data for all items of a given merchant' do
+    merchant1 = create(:merchant)
+    merchant2 = create(:merchant)
+
+    create_list(:item, 2, merchant_id: merchant1.id)
+		create_list(:item, 2, merchant_id: merchant2.id)
+
+    get "/api/v1/merchants/#{merchant1.id}/items"
+
+		merchant1_items = JSON.parse(response.body, symbolize_names: true)
+    
+		expect(response).to be_successful
+		expect(merchant1_items.count).to eq(1)
+		expect(merchant1_items[:data].first).to have_key(:id)
+		expect(merchant1_items[:data].first[:attributes]).to have_key(:name)
+		expect(merchant1_items[:data].first[:attributes]).to have_key(:description)
+		expect(merchant1_items[:data].first[:attributes]).to have_key(:unit_price)
+		expect(merchant1_items[:data].first[:merchant_id]).to_not eq(merchant2.id)
   end
 end
