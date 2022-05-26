27142e6ea3f7c8e72ce77d91e4206e6b3b0eccee
John Hennerich <john.hennerich@gmail.com>
2022-05-26 13:46:46 -0600

    Feat: add table relation to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,20 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
+  has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
+
+  # def self.top_merchants_by_revenue(quantity)
+  #   Merchant
+  #     .select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) as revenue')
+  #     .joins(invoices: [:invoice_items, :transactions])
+  #     .group(:id)
+  #     .where(transactions: {result: 'sucess'}, invoices: status: 'shipped'})
+  #     .order(revenue: :desc)
+  #     .limit(quantity)
+  # end
 
   def self.search_return_one(search_params)
     where("name ILIKE ?", "%#{search_params}%").order(:name).first
